58191a8468c9d557d1e5328789a444c0950a676d
railosmith <railosmith@garage.(none)>
2010-07-21 00:27:20 +0530

    making it work on heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ validates_numericality_of :income
 validates_numericality_of :age
 #validates_presence_of :image
 belongs_to :group
-file_column :image , :magick => { :geometry => "150x150" }
+#file_column :image , :magick => { :geometry => "150x150" }
 has_and_belongs_to_many :ledgers
 has_and_belongs_to_many :refunds
 has_many :ledgers_users
